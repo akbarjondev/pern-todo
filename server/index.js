47730e8a6c4de88e2ef8178f60100a26ccdae508
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-01-30 23:18:21 +0500

    Add create todo [post request]

--- server/index.js
+++ server/index.js
@@ -1,7 +1,29 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const CONFIG = require('./src/config')
+const pool = require('./src/db')
 
 
+// middleware
+app.use(cors())
+app.use(express.json())
 
-app.listen(CONFIG.PORT, () => console.log(`Ready at http://localhost:${CONFIG.PORT}`))
+
+//ROUTES//
+
+// create a todo
+app.post('/todos', async (req, res) => {
+	try {
+		const { description } = req.body
+
+		const newTodo = await pool.query(
+			'INSERT INTO todo (description) VALUES($1) RETURNING *',
+			[description]
+		)
+
+		res.json(newTodo.rows[0])
+	} catch(e) {
+		console.log(e.message);
+	}
+})
