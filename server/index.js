52afc46e021d5f499e1a4b6c149f1dca6101e4a9
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-02-11 16:21:48 +0500

    Add edit todo

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.post('/todos', async (req, res) => {
 	}
 })
 
-// get a todo
+// get all todos
 app.get('/todos', async (req, res) => {
 	try {
 		const allTodos = await pool.query('SELECT * FROM todo')
@@ -39,8 +39,37 @@ app.get('/todos', async (req, res) => {
 	}
 })
 
+// get a todo
+app.get('/todos/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		const todo = await pool.query(
+			'SELECT * FROM todo WHERE todo_id = $1',
+			[id]
+		)
+
+		res.json(todo.rows[0])
+	} catch(e) {
+		console.log(e.message);
+	}
+})
+
 
 // update a todo
+app.put('/todos/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		const { description } = req.body
+		const updateTodo = pool.query(
+			'UPDATE todo SET description = $1 WHERE todo_id = $2',
+			[description, id]
+		)
+
+		res.json(updateTodo.rows)
+	} catch(e) {
+		console.log(e.message);
+	}
+})
 
 // delete a todo
 
